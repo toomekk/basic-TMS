9138964679fb7fec10794ade587c1313d99e5650
Tomasz Kocyła <kocyla.t@gmail.com>
2023-05-13 15:07:38 +0200

    Initial commit

---
+++ src/main/java/com/example/basictms/BasicTmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.basictms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicTmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/basictms/BasicTmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basictms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicTmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
